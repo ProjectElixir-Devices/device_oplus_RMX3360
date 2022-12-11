a2cb29f7bd80fcd3954b1647e83418dbf85c1ec4
itsXRP <itsxrproms@gmail.com>
2022-12-11 07:48:20 +0000

    RMX3360: Switch to aosp overlay
    Signed-off-by: itsXRP <itsxrproms@gmail.com>

--- device.mk
+++ device.mk
@@ -31,7 +31,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aosp
 
 PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
