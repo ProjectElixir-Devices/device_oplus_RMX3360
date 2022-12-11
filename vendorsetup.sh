9bd2c68ba5ffa1cf003c4b7205a41347b916bf95
itsXRP <itsxrproms@gmail.com>
2022-12-11 07:58:50 +0000

    RMX3360: Include vendorsetup.sh
    Signed-off-by: itsXRP <itsxrproms@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+#Clone vibrator HAL
+rm -rf vendor/qcom/opensource/vibrator
+git clone https://github.com/itsxrp/android_vendor_qcom_opensource_vibrator.git vendor/qcom/opensource/vibrator
+
+#Clone Audio Hal
+rm -rf hardware/qcom-caf/sm8350/audio
+git clone https://github.com/itsxrp/android_hardware_qcom_audio.git hardware/qcom-caf/sm8350/audio
+
